25c6f7d2796841c9b2aea42f0a4e14017e71d0e8
Raykosan <71210053+Raykosan@users.noreply.github.com>
2026-03-24 22:46:30 +0300

    Added new node - RS Loop Switch

---
+++ web/loopSwitchSeed.js
@@ -0,0 +1,30 @@
+import { app } from "../../scripts/app.js";
+
+app.registerExtension({
+    name: "RaykoStudio.LoopSwitchSeed",
+    
+    async beforeRegisterNodeDef(nodeType, nodeData, app) {
+        if (nodeData.name === "RaykoLoopSwitchSeed") {
+            
+            nodeType.prototype.onExecuted = function(message) {
+                // В ComfyUI 0.18+ данные могут приходить прямо в message
+                const uiData = message.ui || message;
+                
+                if (!this.widgets || !uiData) {
+                    return;
+                }
+                
+                for (let i = 1; i <= 10; i++) {
+                    const widgetName = "value_" + i;
+                    const widget = this.widgets.find(w => w.name === widgetName);
+                    
+                    if (widget && uiData[widgetName]) {
+                        widget.value = uiData[widgetName][0];
+                    }
+                }
+                
+                this.setDirtyCanvas(true, true);
+            };
+        }
+    }
+});
